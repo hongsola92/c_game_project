9740bbb6693a5b208fc296bf8518fdf7c2abfe87
hongsola92 <thfdkwl@kakao.com>
2024-03-19 17:25:50 +0900

    tetris 일부

---
+++ src/display_menu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int display_menu()
+{
+    int menu;
+
+    system("clear");
+
+    printf("     Tetris gsa\n");
+    printf("-------------------\n");
+    printf("     GAME MENU\n");
+    printf("-------------------\n");
+    printf("1. ＊Game Start＊\n");
+    printf("2. ＊Search history＊\n");
+    printf("3. ＊Record Output＊\n");
+    printf("4. ＊Quit＊\n");
+
+    scanf("%d", &menu);
+    return menu;
+}
---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int display_menu();
+void display_tetris();
+
+int main()
+{
+    int menu = 1;
+    menu = display_menu();
+    switch (menu)
+    {
+    case 1:
+        printf("＊Game Start＊\n");
+        display_tetris();
+        break;
+    case 2:
+        printf("＊Search history＊\n");
+        break;
+    case 3:
+        printf("＊Record Output＊\n");
+        break;
+    case 4:
+        printf("＊Quit＊\n");
+        break;
+    }
+    return 0;
+}
