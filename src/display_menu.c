f48387d32e5b06984701847d27258b0e3c2a9195
hongsola92 <thfdkwl@kakao.com>
2024-03-19 17:26:10 +0900

    .

--- src/display_menu.c
+++ src/display_menu.c
@@ -7,14 +7,14 @@ int display_menu()
 
     system("clear");
 
-    printf("     Tetris gsa\n");
+    printf("Tetris hsa\n");
     printf("-------------------\n");
-    printf("     GAME MENU\n");
+    printf("\t\tGAME MENU\n");
     printf("-------------------\n");
-    printf("1. ＊Game Start＊\n");
-    printf("2. ＊Search history＊\n");
-    printf("3. ＊Record Output＊\n");
-    printf("4. ＊Quit＊\n");
+    printf("1. Game Start\n");
+    printf("2. Search history\n");
+    printf("3. Record Output\n");
+    printf("4. Quit\n");
 
     scanf("%d", &menu);
     return menu;
--- src/display_tetris.c
+++ src/display_tetris.c
@@ -1,48 +1,21 @@
-#include "display_tetris.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void display_tetris()
+int display_menu()
 {
+    int menu;
+
     system("clear");
-    printf("Next Block\n");
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            if (block[i][j] == 0)
-            {
-                printf("o ");
-            }
-            else
-            {
-                printf("* ");
-            }
-        }
-        printf("\n");
-    }
-    printf("\n\n");
-    printf("Tetris Table\n");
-    for (int i = 2; i < 21; i++)
-    {
-        printf("\t");
-        for (int j = 0; j < 10; j++)
-        {
-            if (j == 0 || j == 9)
-            {
-                printf("|");
-            }
-            else if (i == 20 && (j > 1 || j < 9))
-            {
-                printf("-");
-            }
-            else if (tetris_table[j][i] == 0)
-            {
-                printf("o");
-            }
-            else if (tetris_table[j][i] == 1)
-            {
-                printf("*");
-            }
-        }
-        printf("\n");
-    }
+
+    printf("Tetris csk\n");
+    printf("-------------------\n");
+    printf("\t\tGAME MENU\n");
+    printf("-------------------\n");
+    printf("1. Game Start\n");
+    printf("2. Search history\n");
+    printf("3. Record Output\n");
+    printf("4. Quit\n");
+
+    scanf("%d", &menu);
+    return menu;
 }
