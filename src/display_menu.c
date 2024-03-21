e3f91bd5d63f5301c370e009e361b74997538482
mifasol_a <thfdkwl@kakao.com>
2024-03-21 11:10:00 +0900

    tetris

--- src/display_menu.c
+++ src/display_menu.c
@@ -7,14 +7,14 @@ int display_menu()
 
     system("clear");
 
-    printf("Tetris hsa\n");
+    printf("＊Tetris hsa＊\n");
     printf("-------------------\n");
-    printf("\t\tGAME MENU\n");
+    printf("     ＊GAME MENU＊\n");
     printf("-------------------\n");
-    printf("1. Game Start\n");
-    printf("2. Search history\n");
-    printf("3. Record Output\n");
-    printf("4. Quit\n");
+    printf("1. ＊Game Start＊\n");
+    printf("2. ＊Search history＊\n");
+    printf("3. ＊Record Output＊\n");
+    printf("4. ＊Quit＊\n");
 
     scanf("%d", &menu);
     return menu;
--- src/display_tetris.c
+++ src/display_tetris.c
@@ -1,21 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "display_tetris.h"
+extern int i_block[4][4][4];
+extern int t_block[4][4][4];
+extern int s_block[4][4][4];
+extern int z_block[4][4][4];
+extern int l_block[4][4][4];
+extern int j_block[4][4][4];
+extern int o_block[4][4][4];
+extern int next_block_number;
 
-int display_menu()
+void display_tetris()
 {
-    int menu;
+    int i, j;
+    int(*block_pointer)[4][4][4] = NULL;
 
+    switch (next_block_number)
+    {
+    case 0:
+        block_pointer = &i_block;
+        break;
+    case 1:
+        block_pointer = &t_block;
+        break;
+    case 2:
+        block_pointer = &s_block;
+        break;
+    case 3:
+        block_pointer = &z_block;
+        break;
+    case 4:
+        block_pointer = &l_block;
+        break;
+    case 5:
+        block_pointer = &j_block;
+        break;
+    case 6:
+        block_pointer = &o_block;
+        break;
+    }
     system("clear");
-
-    printf("Tetris csk\n");
-    printf("-------------------\n");
-    printf("\t\tGAME MENU\n");
-    printf("-------------------\n");
-    printf("1. Game Start\n");
-    printf("2. Search history\n");
-    printf("3. Record Output\n");
-    printf("4. Quit\n");
-
-    scanf("%d", &menu);
-    return menu;
+    printf("Next Block\n");
+    for (i = 0; i < 4; i++)
+    {
+        printf("\n");
+        for (j = 0; j < 4; j++)
+        {
+            if ((*block_pointer)[0][i][j] == 1)
+                printf("* ");
+            else if ((*block_pointer)[0][i][j] == 0)
+                printf("o ");
+        }
+    }
+    printf("\n\n");
+    printf("Tetris Table\n");
+    for (i = 2; i < 21; i++)
+    {
+        printf("\t");
+        for (j = 0; j < 10; j++)
+        {
+            if (j == 0 || j == 9)
+            {
+                printf("│ ");
+            }
+            else if (i == 20 && (j > 1 || j < 9))
+            {
+                printf("──");
+            }
+            else if (tetris_table[i][j] == 1)
+                printf("▒ ");
+            else if (tetris_table[i][j] == 0)
+                printf("  ");
+        }
+        printf("\n");
+    }
 }
